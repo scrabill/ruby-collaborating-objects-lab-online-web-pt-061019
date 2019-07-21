02beb297ef5816befecafa6a789f79070c6caf5e
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-21 21:27:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 require "pry"
 class Song
   attr_accessor :name, :artist, :genre
+=======
+class Song
+  attr_accessor :name, :artist
+>>>>>>> 0b8afda17dc498daa16699a8bd5b12cea43905ca
 
   def initialize(name)
     @name = name
@@ -10,6 +15,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
+<<<<<<< HEAD
     # binding.pry
     x = filename.gsub(".mp3","").split(" - ")
     artist = x[0]
@@ -24,12 +30,20 @@ class Song
     # self.artist.name = artist
     # binding.pry
     # song
+=======
+    name = filename.split(" - ")[0]
+    artist_name = filename.split(" - ")[1]
+    song = Song.new(name)
+    song.artist = artist_name
+    song
+>>>>>>> 0b8afda17dc498daa16699a8bd5b12cea43905ca
   end
   #
   # def artist=(artist_name)
   #   artist_name = self.artist.name
   # end
 
+<<<<<<< HEAD
   # def name
   #   # self.artist.name = name
   # end
@@ -37,6 +51,11 @@ class Song
 
   def artist
     self.artist = @artist
+=======
+
+  def artist
+    self.artist == artist
+>>>>>>> 0b8afda17dc498daa16699a8bd5b12cea43905ca
   end
 
 end
